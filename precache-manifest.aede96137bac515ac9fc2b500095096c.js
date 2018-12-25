cd3f2077676ecd6c54bf7f8bded25b12ae1bc008
Karthik <amrkarthik@outlook.com>
2018-12-25 13:43:18 +0530

    Updates

---
+++ precache-manifest.aede96137bac515ac9fc2b500095096c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-php/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "edbe20cc6be111c40c52",
+    "url": "/react-php/static/js/runtime~main.edbe20cc.js"
+  },
+  {
+    "revision": "002fbdd52e15f7d04a56",
+    "url": "/react-php/static/js/main.002fbdd5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-php/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "002fbdd52e15f7d04a56",
+    "url": "/react-php/static/css/main.1ee35020.chunk.css"
+  },
+  {
+    "revision": "728d41bc758b2e1b22eaad29814ee9a3",
+    "url": "/react-php/index.html"
+  }
+];
